fd91c62911ebdeb04a4cbe2a847c75c5bddac050
Joshua Fairchild and Nick Wise <joshua+nick@>
2017-06-29 19:49:21 -0700

    Twitter bot for posting, searching, and getting random text out of the array of strings.

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const apiKeys = require("./.env").apiKeys;
-
 const Twit = require('twit');
-const happyText = require("./");
+const happyText = require("./happy-text.json");
 
 
 class Bot {
@@ -13,10 +12,29 @@ class Bot {
 
     this.api.post('statuses/update', {status: content})
       .then(console.log(`Success: ${content}`))
-      .catch(error => console.log(`failed: ${error.stack}`))
+      .catch(error => console.log(`failed: ${error.stack}`));
 
   }
+
+  searchAndReply() {
+    this.api.get('search/tweets', {q: "sad", count: 1})
+      .then(response => {
+        const tweetId = response.data.statuses[0].id_str;
+        const username = response.data.statuses[0].user.screen_name
+        this.api.post('statuses/update', {in_reply_to_status_id: tweetId, status: `${this.getRandomText(happyText)} @${username}`})
+          .then(console.log(`Success:`))
+          .catch(error => console.log(error.stack));
+      })
+      .catch(error => console.log(error));
+  }
+
+  getRandomText(collection) {
+    return collection[Math.floor(Math.random()*((collection.length-1)-0+1)+0)];
+  }
+
 }
 
 const bot = new Bot();
-bot.postTweet('words');
+
+// setInterval(() => bot.searchAndReply(), 1000*5);
+bot.searchAndReply();
